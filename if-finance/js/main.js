b8ce3cf459d8b97e159a5a43d9a09b50612d26b3
Duda <ifeduarda.luiz@gmail.com>
2024-11-14 22:16:58 -0300

    novas funçoes

--- if-finance/js/main.js
+++ if-finance/js/main.js
@@ -1,13 +1,50 @@
-const flex= () =>{
+const openModal= () =>{
     const modal = document.querySelector('.modal')
     modal.style.display= 'flex'
 }
-const fechar= (event)=>{
-    const fecha = document.querySelector('.modal')
-    console.log(event.target.Classname)
-    if(event.target.Classname ==='modal')
-    fecha.style.display='none'
-    if(event.target.Classname=== undefined)
-    fecha.style.display='none'
+const closeModal= (event)=>{
+    const modal = document.querySelector('.modal')
 
+    // if(event === undefined)
+    //     return modal.style.display= 'none'
+    
+    // if(event.target.className ==='modal')
+    //      return modal.style.display= 'none'
+    
+    if(event?.target?.className ==='modal')
+        return modal.style.display= 'none'
+    
+    if(event === undefined)
+        return modal.style.display= 'none'
+}
+
+
+const handleSubmitAddTicker = (event) =>{
+    //Previne que a pagina de recarregar, ao apetar o botão
+    event.preventDefault()
+    // Valores do form
+    const urlLogo = event.target.urlLogo.value
+    const nameCompany = event.target.nameCompany.value
+    const ticker = event.target.ticker.value
+    const quantity = event.target.quantity.value
+    const closeValue = event.target.closeValue.value
+    // card
+    const cardList = document.getElementById('card-list')
+    cardList.innerHTML += `
+            <div class="card">
+                <header>
+                    <img src="${urlLogo}" alt="Logo">
+                    <h4>${nameCompany}</h4>
+                    <span>${ticker}</span>
+                </header>
+                <main>
+                    <p>Valor: <span style="color: lime;">${closeValue}<span>▲▼</span></p>
+                </main>
+                <footer>
+                    <p>Quantidade: <span>${quantity}</span></p>
+                    <p>Posição: <span>${quantity * parseFloat(closeValue)}</span></p>
+                </footer>
+            </div>`
+    closeModal()
+    event.target.reset()
 }
